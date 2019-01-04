fe989c3e6e5d63d91a62d125f7a3e99e664e2185
Rodolfo Brian <rodolfo_bdf@hotmail.com>
2019-01-04 00:25:58 +0000

    bank account

---
+++ BankAccount.java
@@ -0,0 +1,40 @@
+
+public class BankAccount {
+    
+    public BankAccount(int anAccountNumber) {
+
+        accountNumber = anAccountNumber;
+        balance = 0;
+    }
+
+    public BankAccount (int anAccountNumber, double initialBalance) {
+
+        accountNumber = anAccountNumber;
+        balance = initialBalance;
+    }
+
+    public  int getAccountNumber()
+    {
+        return accountNumber;
+    }
+
+    public void deposit(double amount) {
+
+        double newBalance = balance + amount;
+        balance = newBalance;
+    }
+
+    public void withdraw(double amount) {
+
+        double newBalance = balance - amount;
+        balance = newBalance;
+    }
+
+    public double getBalance() {
+    
+        return balance;
+    }
+
+    private int accountNumber;
+    private double balance;
+}
